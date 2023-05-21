179e05e7599f170cc11dde48b7afc655d9ee0e11
xiang-921101 <132485665+xiang-921101@users.noreply.github.com>
2023-05-21 17:20:09 +0800

    123

--- main.py
+++ main.py
@@ -2,3 +2,49 @@
 目的：主函式撰寫
 負責組員：李湘菱（主要架構）、周詠熙（html與flask框架整合）
 """
+
+#最開始的系統介面
+def showstartmenu():
+    
+    while(True):
+        print("歡迎來到此系統")
+        print("1)員工\n2)管理員\n0)離開")
+        try:
+            num = int(input("請選擇您的身分："))
+            if (num>0)and(num<2):
+                return num
+            else:
+                print("請輸入[1,2,0]\n ")
+        except ValueError:
+            print("請輸入整數\n ")
+
+#員工介面
+def showemployeemenu():
+    
+    while(True):
+        print("歡迎xxx")
+        print("1)看班表\n2)輸入上班紀錄\n3)本月薪資\n0)離開")
+        try:
+            num = int(input("請選擇要做的事："))
+            if (num>0)and(num<3):
+                return num
+            else:
+                print("請輸入[1,2,3,0]\n ")
+        except ValueError:
+            print("請輸入整數\n ")
+
+#管理員介面
+def showadminmenu():
+    
+    while(True):
+        print("歡迎xx管理員")
+        print("1)看部門班表\n2)上傳新的班表\n3)員工資料\n4)設定\n0)離開")
+        try:
+            num = int(input("請選擇要做的事："))
+            if (num>0)and(num<4):
+                return num
+            else:
+                print("請輸入[1,2,3,4,0]\n ")
+        except ValueError:
+            print("請輸入整數\n ")
+
--- schedule.py
+++ schedule.py
@@ -2,3 +2,23 @@
 目的：班表輸入與輸出、更改
 負責組員：李湘菱
 """
+import csv
+
+def read_csv_file(file_path):
+    data=[]
+    with open(file_path,'r') as file:
+        csv_reader=csv.reader(file)
+        for row in csv_reader:
+            data.append([value for value in row])
+    return data
+#!!!
+def getschedulw(name,total):
+    
+    for i in total:
+        if (i[0]=='day') or (i[1]==name):
+            print(i[0],i[1],i[2],i[3],sep="   ")
+        
+
+file_path='schedule.csv'
+total=read_csv_file(file_path)
+getschedulw('Megan',total)

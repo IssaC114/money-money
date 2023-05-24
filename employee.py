2791a152945c70f68612e41a9c87f4c32bd03048
xiang-921101 <132485665+xiang-921101@users.noreply.github.com>
2023-05-24 23:46:28 +0800

    132

--- employee.py
+++ employee.py
@@ -6,7 +6,7 @@
 import csv
 
 import schedule
-'''  物件(但真的有需要嗎)
+
 class Employee:
     def __init__(self,name,salary) :
         self.name=name
@@ -15,9 +15,8 @@ class Employee:
     def calculate_salary(self):
         return self.salary*schedule(self.name)
 
-'''
 
-def get_salary(basic_salary,name):
-    return basic_salary*schedule.gettotaltime(name)
+
+
 
 

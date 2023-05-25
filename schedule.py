d2a0c330b3005c6ce0c8e8dccd0b5ab00c89b006
xiang-921101 <132485665+xiang-921101@users.noreply.github.com>
2023-05-25 14:39:25 +0800

    132

--- schedule.py
+++ schedule.py
@@ -3,6 +3,7 @@
 負責組員：李湘菱
 """
 import csv
+import datetime
 
 #讀檔並轉二維陣列
 def read_csv_file(file_path):
@@ -38,6 +39,43 @@ def getverify():
     name=[row[1] for row in data]
     return name
 
+def calculate_total_hours(target_month , target_year):
+    #取現實年月
+    current_month = datetime.datetime.now().month
+    current_year = datetime.datetime.now().year
 
+    #取當月所有日期
+    first_day = datetime.datetime(target_year,target_month,1)
+    next_month = first_day.replace(day=28) + datetime.timedelta(days=4)
+    last_day = next_month - datetime.timedelta(days = 1)
+    all_dates = [first_day + datetime.timedelta(days=x) for x in range((last_day - first_day).days + 1)]
 
+    total_hours = {}
 
+    for employee_schedule in read_csv_file('uploaded_schedule.csv'):
+        employee = employee_schedule['name']
+
+        if employee not in total_hours:
+            total_hours[employee] = []
+    
+        hours = 0
+
+        for date in all_dates:
+            day = date.strftime("%A")
+
+            if day == employee_schedule['day']:
+                if date == datetime.datetime.now() and date.month == current_month and date.year == current_year:
+                    start_time = datetime.datetime(date.year , date.month , date.day , float(employee_schedule['start']))
+                    end_time = datetime.datetime.now()
+                    diff = end_time - start_time
+                    working_hours = diff.total_seconds() / 3600
+                else:
+                    # 非當前日期，計算整天的工作時數
+                    working_hours = float(employee_schedule["end"]) - float(employee_schedule["start"])
+
+                hours += working_hours
+
+        total_hours[employee].append(hours)
+    return total_hours
+
+calculate_total_hours(5,2023)
